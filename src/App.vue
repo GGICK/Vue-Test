a42bb3934dc6a8d428b56ccbc13b78926b025952
gyu <qkr9748200@gmail.com>
2024-10-15 17:09:07 +0900

    Create VueBase

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <SayHello />
+</template>
+
+<script>
+import SayHello from "~/components/SayHello";
+
+export default {
+  data() {
+    return {
+      msg: "hello webpack!"
+    };
+  },
+  components: {
+    SayHello
+  }
+};
+</script>
